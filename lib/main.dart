c9ce05e0c16a85dcf42d355904d135a645a1a532
Mohammad <mohammadwork199700@gmail.com>
2025-05-29 05:33:04 +0000

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-// loadSplashScreen();
+// loadSplashScreen();,
   setupGetIt();
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
